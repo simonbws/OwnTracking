d4b8e2709356ada8c05046bd3acd9e55dd32dde7
szymon <DESKTOP-C0TU3RB>
2022-10-28 23:25:03 +0200

    Positions Form Design part2

---
+++ OwnTracking/FrmPositionList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OwnTracking
+{
+    public partial class FrmPositionList : Form
+    {
+        public FrmPositionList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
